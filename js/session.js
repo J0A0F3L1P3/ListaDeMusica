8e12ff92da23c342b17f0f7d2a3f4c15c21e78a1
João Felipe <85209912+J0A0F3L1P3@users.noreply.github.com>
2024-03-15 00:12:53 -0300

    Update session.js

--- js/session.js
+++ js/session.js
@@ -15,7 +15,7 @@ auth.onAuthStateChanged(firebaseUser => {
     }
     else {
         dados.style.display = "none"
-        window.location.pathname="/html/index.html"
+        window.location.pathname="index.html"
     }
     
 })
