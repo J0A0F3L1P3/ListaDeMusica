dc17b909f9000a58569feb14d558c20bcd6b7002
João Felipe <85209912+J0A0F3L1P3@users.noreply.github.com>
2024-03-14 23:41:50 -0300

    create a new repositore

---
+++ js/session.js
@@ -0,0 +1,26 @@
+let dados = document.querySelector("#dados")
+let perfil = document.querySelector("#perfil")
+let foto = document.querySelector("#foto")
+
+// controle de sessão.
+auth.onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+        console.log(auth.currentUser.email + " logado" )
+        dados.style.display = "block"
+        perfil.innerHTML = auth.currentUser.email
+        
+        if (auth.currentUser.photoURL) {
+            foto.innerHTML = "<img src='"+ auth.currentUser.photoURL + "'>"
+        }
+    }
+    else {
+        dados.style.display = "none"
+        window.location.pathname="/html/index.html"
+    }
+    
+})
+
+let btnSair = document.querySelector("#btnSair")
+btnSair.addEventListener("click", () => {
+   auth.signOut() 
+})
