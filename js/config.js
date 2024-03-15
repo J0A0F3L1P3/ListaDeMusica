62666560652c3b40599aeb0052a6842dc61fc399
João Felipe <85209912+J0A0F3L1P3@users.noreply.github.com>
2024-03-15 00:28:30 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,18 +5,6 @@
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyDK_wUdrvSWOIfBE5KKwldIjhHijJb4wtc",
-  //   authDomain: "etecpw3-e485e.firebaseapp.com",
-  //   databaseURL: "https://etecpw3-e485e-default-rtdb.firebaseio.com",
-  //   projectId: "etecpw3-e485e",
-  //   storageBucket: "etecpw3-e485e.appspot.com",
-  //   messagingSenderId: "461646732121",
-  //   appId: "1:461646732121:web:9fe088abf484ac56474a48"
-  // };
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB8890sv0YPrN1UZ6o94oxwgz-povl02D8",
   authDomain: "etec24-d5e05.firebaseapp.com",
