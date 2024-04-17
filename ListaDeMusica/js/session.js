955fcd0f0d744e674f2de3231c8058072e9ef9ec
João Felipe <85209912+J0A0F3L1P3@users.noreply.github.com>
2024-04-17 14:49:02 -0300

    Add files via upload

---
+++ ListaDeMusica/js/session.js
@@ -0,0 +1,24 @@
+// joao
+
+let perfil = document.querySelector("#perfil")
+let foto = document.querySelector("#foto")
+
+auth.onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        // console.log(auth.currentUser.email + " logado")
+        perfil.innerHTML = auth.currentUser.email
+        // + " " + auth.currentUser.displayName
+    if (auth.currentUser.photoURL) {
+        foto.innerHTML = "<img src='" + auth.currentUser.photoURL + "'>"
+    }
+    }
+    else {
+        // window.location.pathname = "/index.html"
+        window.location.pathname="/ListaDeMusica/index.html"
+    }
+})
+
+let btnSair = document.querySelector("#btnSair")
+btnSair.addEventListener("click", () => {
+    auth.signOut()
+})
